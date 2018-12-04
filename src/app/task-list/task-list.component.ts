0c9ddc648bc8a5d9e69a9ab12725616c89ced4c7
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2018-12-04 15:54:12 -0300

    Not allowing empty tasks anymore

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -19,7 +19,7 @@ export class TaskListComponent implements OnInit {
 
   constructor() {
     let localTasks: string = localStorage.getItem('tasks');
-    if ( localTasks != null )
+    if (localTasks != null)
       this.tasks = JSON.parse(localStorage.getItem('tasks'));
   }
 
@@ -28,24 +28,28 @@ export class TaskListComponent implements OnInit {
   }
 
   saveTask() {
-    this.tasks.push({ id: this.generateUniqueId(), name: this.taskInput, done: false });
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    this.taskInput = '';
+    if (this.taskInput !== '') {
+      this.tasks.push({ id: this.generateUniqueId(), name: this.taskInput, done: false });
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      this.taskInput = '';
+    } else {
+      alert('Task must not be empty!');
+    }
   }
 
   closeTask(t: Task) {
-    this.tasks = this.tasks.filter( (task: Task) => task.id != t.id );
+    this.tasks = this.tasks.filter((task: Task) => task.id != t.id);
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   private generateUniqueId() {
-      var d = new Date().getTime();
-      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = (d + Math.random()*16)%16 | 0;
-          d = Math.floor(d/16);
-          return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-      });
-      return uuid;
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
   }
 
 }
